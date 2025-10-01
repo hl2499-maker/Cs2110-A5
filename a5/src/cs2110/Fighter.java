e694b84a7f11fcaad7b8808838266a993866d44a
RealStongy <102835628+RealStongy@users.noreply.github.com>
2025-10-01 19:40:38 -0400

    Fighter class

--- a5/src/cs2110/Fighter.java
+++ a5/src/cs2110/Fighter.java
@@ -1,9 +1,60 @@
 package cs2110;
 
 public class Fighter extends Player {
-    private int health;
-    public Fighter() {
-        super();
+    private Weapon equipped;
+
+    public Fighter(String name, GameEngine engine) {
+        super(name, engine);
+        this.equipped = null;
     }
 
+    @Override
+    public int power() {
+        int weapon_power = 0;
+        if (equipped != null) {
+            weapon_power += equipped.power();
+        }
+        return weapon_power + super.power();
+    }
+
+    @Override
+    public int toughness() {
+        int weapon_toughness = 0;
+        if (equipped != null) {
+            weapon_toughness += equipped.toughness();
+        }
+        return weapon_toughness + super.toughness();
+    }
+
+    public boolean chooseAction() {
+        System.out.print("Would you like to change your current equipment (yes/no)? ");
+        String input = engine.getInputLine();
+
+        if (input.equals("yes")) {
+            Weapon chosen = engine.selectWeapon();
+            if (chosen == null) {
+                if (equipped != null) {
+                    equipped.unequip();
+                    equipped = null;
+                }
+            } else {
+                if (equipped != null) {
+                    equipped.unequip();
+                }
+                chosen.equip();
+                equipped = chosen;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    protected void processDeath() {
+        if (equipped != null) {
+            equipped.unequip();
+            equipped = null;
+        }
+        super.processDeath();
+    }
+}
 
