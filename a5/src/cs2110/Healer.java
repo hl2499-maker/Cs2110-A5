7a399507614237d06f2e8ac7173aceb868baffd7
RealStongy <102835628+RealStongy@users.noreply.github.com>
2025-10-01 21:04:15 -0400

    2

--- a5/src/cs2110/Healer.java
+++ a5/src/cs2110/Healer.java
@@ -1,9 +1,17 @@
 package cs2110;
 
 public class Healer extends Mage {
+
     private String Spellname = "heal spell";
-    public Healer(String name,GameEngine engine) {
-        super(name,engine);
+
+    public Healer(String name, GameEngine engine) {
+        super(name, engine);
+    }
+
+    @Override
+    protected void useSpell() {
+        int healRoll = engine.diceRoll(0, power());
+        heal(healRoll);
     }
 
 }
--- a5/src/cs2110/Mage.java
+++ a5/src/cs2110/Mage.java
@@ -1,19 +1,23 @@
 package cs2110;
 
 public abstract class Mage extends Player {
+
     private String Spellname;
-    private boolean willcast = false;
+//    private boolean willcast;
+
     public Mage(String name, GameEngine engine) {
         super(name, engine);
 
     }
+
     public boolean chooseAction() {
-        System.out.print("Would you like to cast a"+this.Spellname+" ");
-        String indicator= engine.getInputLine();
+        System.out.print("Would you like to cast a" + this.Spellname + " ");
+        String indicator = engine.getInputLine();
         if (indicator.equals("yes")) {
-            willcast = true;
+            return true;
         }
         return false;
     }
 
+    protected abstract void useSpell();
 }
